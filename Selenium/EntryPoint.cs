5ecde6f9e5ab9cb9f537ab5bf8438dccc9ac9f9b
yuliyanechet <yuliyanechet@yandex.ua>
2018-04-11 17:59:22 +0300

    first

---
+++ Selenium/EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace Selenium
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            string jobSelectorButtonXPath = @"//*[@id=""navbarsExampleDefault""]/ul/li[1]/a";
+            string inputFieldXCSSPath = "body > app-root > app-job-selector > div > label:nth-child(4) > input";
+            string setButtonXPath = "/html/body/app-root/app-job-selector/div/label[2]/button";
+            
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://vc1tsw01tfs15v7:8080/welcome");
+            IWebElement jobSelectorButton = driver.FindElement(By.XPath(jobSelectorButtonXPath));
+            jobSelectorButton.Click();
+            IWebElement inputField = driver.FindElement(By.CssSelector(inputFieldXCSSPath));
+            inputField.SendKeys("Some text");
+            IWebElement setButton = driver.FindElement(By.XPath(jobSelectorButtonXPath));
+            setButton.Click();
+
+
+
+
+
+            Thread.Sleep(30000);
+
+            driver.Quit();
+        }
+    }
+}
